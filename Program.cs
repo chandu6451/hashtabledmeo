48edf77753265fde99c7fb16e7e0519d16b91970
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 14:53:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HashTableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+
+            ht.Add(1, "Mumbai");
+            ht.Add(2, "Pune");
+            ht.Add(3, "Hyderabad");
+            ht.Add(4, "Pune");
+            ht.Add(5, null);
+            ht.Add(6, "Chennai");
+
+            Console.WriteLine("HashTable Data : ");
+            foreach (object key in ht.Keys)
+            {
+                Console.Write("Key : " + key + "\t");
+                Console.WriteLine("Value : " + ht[key]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
